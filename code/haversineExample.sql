b02af65dfb95594306e87ebc75126924a85e8cf3
Chip Oglesby <chip@creativesparkcolumbia.com>
2018-04-25 11:54:43 -0500

    Updating haversine code

---
+++ code/haversineExample.sql
@@ -0,0 +1,26 @@
+SELECT
+  latitude,
+  longitude,
+  date,
+  (69.0 *
+    DEGREES(ACOS(COS(RADIANS(latitudeLag)) *
+    COS(RADIANS(latitude)) *
+    COS(RADIANS(longitudeLag) -
+    RADIANS(longitude)) +
+    SIN(RADIANS(latitudeLag)) *
+    SIN(RADIANS(latitude))))) haversineDistance,
+FROM (
+  SELECT
+    date,
+    latitudeE7,
+    latitude,
+    longitudeE7 longitude,
+    LAG(latitude) OVER (ORDER BY date ASC) latitudeLag,
+    LAG(longitude) OVER (ORDER BY date ASC) longitudeLag
+  FROM
+    // Change your table name below
+    [xxx.xxx])
+ORDER BY
+  date ASC
+LIMIT
+  10
